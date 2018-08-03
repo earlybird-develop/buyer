9f9aaa66665fb35d43c280812b5f3a8bef51f51d
Rajagopal Subramanian <rajagopal.s@zencode.guru>
2018-08-03 18:31:44 +0530

    Change password, invoice manage and some changes

--- src/app/core/pages/change-password/change-password.page.ts
+++ src/app/core/pages/change-password/change-password.page.ts
@@ -1,23 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../../services';
+import { UserProfile, User } from '../../models';
 
 @Component({
   selector: 'eb-change-password',
   templateUrl: './change-password.page.html',
   styleUrls: ['./change-password.page.scss']
 })
-export class ChangePasswordPage {
+export class ChangePasswordPage implements OnInit{
+
+  public user: UserProfile;
 
   constructor(private _toastr: ToastrService,
               private _accountService: AccountService) {
 
   }
 
-  public changePassword(): void {
+  ngOnInit() {
     this._accountService
-      .changePassword()
-      .subscribe(() => this._toastr.success('Reset Password Email had seen!'));
+      .getProfile()
+      .subscribe(x => this.user = x);
+  }
+
+  public changePassword(): void {
+    // this._accountService
+    //   .changePassword()
+    //   .subscribe(() => this._toastr.success('Reset Password Email had seen!'));
 
   }
 }
