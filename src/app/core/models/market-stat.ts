804656bf53653f5fb3febd6811488a31c535d71d
Loudon <loudonii@163.com>
2018-08-13 16:11:11 +0800

    repaire bug ofr pages's currency sign error

--- src/app/core/models/market-stat.ts
+++ src/app/core/models/market-stat.ts
@@ -2,6 +2,7 @@ import { Alias, Model } from 'tsmodels';
 
 export class MarketStat extends Model {
   @Alias() public currency: string;
+  @Alias('currency_sign') public currencySign: string;
   @Alias('available_amount') public availableAmount: number;
   @Alias('invoice_count') public invoiceCount: number;
   @Alias('suppliers_count') public suppliersCount: number;
